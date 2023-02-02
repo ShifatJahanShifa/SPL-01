1154cb32dca982953e86768744f248a7c083cc41
ShifatJahanShifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2023-02-03 02:23:03 +0600

    Add files via upload

--- header.h
+++ header.h
@@ -1,5 +1,20 @@
 #pragma once
+
+/** this is for built-in header file section **/
+
 #include<iostream>
 #include<ctime>
+
+
+/** this is for source file section **/
+
 #include "BmpStructure.cpp"
+//#include "checkingImageFormat.cpp"
+//#include "checkingTextFile.cpp"
+
+
 using namespace std;
+
+
+int checkingImageFormat(string fileName);
+int checkingTextFile(string imageFile,string textFile);
