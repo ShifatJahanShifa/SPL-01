f5e69724c2de131e5697781e81f6623a7325a3a1
Shifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2023-03-12 00:06:14 +0600

    adding text file extension

---
+++ text file embedding/addTextFileExtension.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include "header.h"
+
+using namespace std;
+
+string addTextFileExtension(string textFileName)
+{
+    textFileName+='.';
+    textFileName+='t';
+    textFileName+='x';
+    textFileName+='t';
+
+    return textFileName;
+}
