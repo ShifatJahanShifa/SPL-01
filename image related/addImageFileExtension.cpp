e76774d71a457647ddd113163740994510abfb0b
Shifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2023-03-12 00:10:33 +0600

    add image file extension

---
+++ image related/addImageFileExtension.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include "header.h"
+
+using namespace std;
+
+string addImageFileExtension(string imageFileName)
+{
+    imageFileName+='.';
+    imageFileName+='b';
+    imageFileName+='m';
+    imageFileName+='p';
+
+    return imageFileName;
+}
