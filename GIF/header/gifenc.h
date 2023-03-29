f77a1e6098ea2b9f1ae26b4de6d4c07f08344757
Shifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2023-03-29 16:26:38 +0600

    header file of gif encoder, (initial commit)

---
+++ GIF/header/gifenc.h
@@ -0,0 +1,34 @@
+#ifndef GIFENC_H
+#define GIFENC_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct GIF {
+    uint16_t widthOfScreen, heightOfScreen;     // width and height total 4 bytes, of the screen
+    int depth;    // color depth
+    int backgroundIndex;    // background color index
+    int fd;         //
+    int offset;     // image info
+    int numberOfFrames;     // frames number
+    uint8_t *frame, *back;    // starting of a frame and ending of a frame
+    uint32_t partial;    //
+    uint8_t buffer[0xFF];    // store
+} GIF;
+
+GIF *newGif(
+    const char *filename, uint16_t width, uint16_t height,
+    uint8_t *palette, int depth, int backgroundIndex, int loop
+);
+void addFrame(GIF *gif, uint16_t delay);
+void closeGif(GIF* gif);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* GIFENC_H */
+
+
