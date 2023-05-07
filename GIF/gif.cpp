1bebf76b7b21c1e07b305adf78830398270ac319
Shifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2023-05-08 01:59:37 +0600

    updated

--- GIF/gif.cpp
+++ GIF/gif.cpp
@@ -1,40 +1,23 @@
-#include "gifenc.h"
+#include "gifencoder.h"
 //#include "header.h"
 #include<stdlib.h>
 #include<stdio.h>
 #include<time.h>
-//#include "gifenc.c"
+#include<bits/stdc++.h>
+using namespace std;
 
-void gif()
+void gif(string pixelFileName,string gifFile)
 {
     int i, j;
     int widthOfScreen = 120, heightOfScreen = 130;
 
 
-    FILE *fp=NULL;
+    //FILE *fp=NULL;
+    FILE *fp=fopen(pixelFileName.c_str(),"rb");
 
     uint8_t colors[24];
-    srand(time(0));
-    int num=rand()%3+1;
-
-    //printf("%d\n",num);
-    //  num=2;
-
-    switch(num)
-    {
-    case 1:
-        fp = fopen("pixel1.BIN", "rb");
-        break;
-
-    case 2:
-        fp = fopen("pixel2.BIN", "rb");
-        break;
-
-    case 3:
-        fp = fopen("pixel3.BIN", "rb");
-        break;
-    }
-
+    //srand(time(0));
+    //int num=rand()%3+1;
 
 
     if (fp == NULL)
@@ -48,12 +31,13 @@ void gif()
     fclose(fp);
 
     GIF *gif = newGif(
-                   "GIF.gif",  widthOfScreen, heightOfScreen, /* canvas size */colors,
+                    gifFile.c_str(),
+                    widthOfScreen, heightOfScreen, /* canvas size */
+                    colors,    /* color pallete */
                     3,              /* palette depth == log2(# of colors) */
                    -1,             /* no transparency */
                     0               /* infinite loop , won't stop*/
                     );
-
 
     /* drawing some frames */
 
