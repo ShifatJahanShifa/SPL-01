fdd903dd534e717e70eec7eb5110f6f36fd222d4
Shifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2023-05-11 20:49:03 +0600

    Final!

--- main and header/main.cpp
+++ main and header/main.cpp
@@ -7,55 +7,49 @@
 #include<time.h>
 #include <thread>
 #include<windows.h>
-
+#include<wincon.h>
+#include<sstream>
 
 using namespace std;
 
 int main()
 {
-    // here beautifying the console output
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
-    WORD savedAttributes;
 
-    // Save the current console attributes
-    GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
-    savedAttributes = consoleInfo.wAttributes;
+    system("color ED");
 
-    // Set the console text color to red
-    SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
-
-    //SetConsoleTextAttribute(hConsole, savedAttributes);
     cout<<"**********************************************";
     cout<<"**************************************************************************\n\n";
-    cout<<"\t\t\t\t\t\tWelcome to Steg-Gif";
-    cout<<"\n\n\n";
+    cout<<"\t\t\t\t  Welcome to Invisible Ink & Animated Frame Encoder";
+    cout<<"\n\n";
     cout<<"**********************************************";
     cout<<"**************************************************************************\n\n";
 
-    SetConsoleTextAttribute(hConsole, savedAttributes);
-    SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
-    cout<<"Here are my project features:";
+    cout<<"\t\t\t\t\t Here are my project features:\n";
+    cout<<"\t\t\t\t\t_______________________________";
     cout<<"\n\n";
-    cout<<"# data hiding";
+    cout<<"\t---------------\t\t";
+    cout<<"\t      -------------------\t\t";
+    cout<<"\t     ---------------\t\t";
     cout<<"\n";
-    cout<<"# data extracting";
+    cout<<"\t| Data Hiding |\t\t";
+    cout<<"\t      | Data Extracting |\t\t";
+    cout<<"\t     | GIF Encoder |\t\t";
     cout<<"\n";
-    cout<<"# GIF encoder";
+    cout<<"\t---------------\t\t";
+    cout<<"\t      -------------------\t\t";
+    cout<<"\t     ---------------\t\t";
     cout<<"\n\n";
-    SetConsoleTextAttribute(hConsole, savedAttributes);
 
     bool continueLoop=true;
 
     while(continueLoop)
     {
-        SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
         cout<<"select an option to proceed: \n\n";
         cout<<"option 1: data hiding\n";
         cout<<"option 2: data extracting\n";
         cout<<"option 3: GIF encoder\n";
         cout<<"option 4: quit\n";
-        SetConsoleTextAttribute(hConsole, savedAttributes);
+
         int choice;
         cin>>choice;
 
@@ -65,20 +59,17 @@ int main()
             string textFileName;
             string extendedTextFileName;
 
-            SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
             cout<<"provide image file name";
             cout<<"\n";
-            SetConsoleTextAttribute(hConsole, savedAttributes);
 
             cin>>imageFileName;
             string extendedImageFileName=addImageFileExtension(imageFileName);
 
-
             // option for text file
             bool looping=true;
+
             while(looping)
             {
-                SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
                 cout<<"how do you want to provide hidden data?";
                 cout<<"\n";
                 cout<<"write now or provide text file name";
@@ -87,7 +78,6 @@ int main()
                 cout<<"\n";
                 cout<<"press 2 for providing text file name";
                 cout<<"\n";
-                SetConsoleTextAttribute(hConsole, savedAttributes);
 
                 int pressingValue;
                 cin>>pressingValue;
@@ -115,10 +105,8 @@ int main()
 
             if(!inputFile)
             {
-                SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY);
                 cout<<"couldn't find the image file in storage\n";
                 cout<<"redirecting to the option menu\n\n";
-                SetConsoleTextAttribute(hConsole,savedAttributes);
                 continue;
             }
 
@@ -127,18 +115,12 @@ int main()
 
             if(!checkingImageFormat(extendedImageFileName))
             {
-                SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY);
                 cout<<"sorry, the image format is not correct.\n";
                 cout<<"redirecting to the option menu.\n\n";
-                SetConsoleTextAttribute(hConsole, savedAttributes);
                 continue;
             }
 
-            //SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE);
-            //cout<<"before hiding data";
-            //cout<<"\n";
             openingImage(extendedImageFileName);
-            //SetConsoleTextAttribute(hConsole, savedAttributes);
 
             /** checking if the text file is compatible for steganography **/   // correction
 
@@ -147,20 +129,16 @@ int main()
 
             if(!inputfile1)
             {
-                SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY);
                 cout<<"couldn't load the text file\n";
                 cout<<"redirecting to the option menu\n\n";
-                SetConsoleTextAttribute(hConsole, savedAttributes);
                 continue;
             }
 
 
             if(!checkingTextFile(extendedImageFileName,extendedTextFileName))
             {
-                SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY);
                 cout<<"not possible to hide the text file within provided image file.";
                 cout<<"redirecting to the option menu.\n\n";
-                SetConsoleTextAttribute(hConsole, savedAttributes);
                 continue;
             }
 
@@ -168,36 +146,25 @@ int main()
 
             if(hidingData(extendedImageFileName,extendedTextFileName))
             {
-                SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
                 puts("stego image is ready");
                 cout<<"\n\n";
-                SetConsoleTextAttribute(hConsole, savedAttributes);
             }
             else
             {
-                SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
                 puts("failed to create stego image");
-                SetConsoleTextAttribute(hConsole, savedAttributes);
                 continue;
             }
 
             Sleep(4000);
-
-            //SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
-            //cout<<"after data hiding";
-            //cout<<"\n";
-            //SetConsoleTextAttribute(hConsole, savedAttributes);
-
             openingImage(extendedImageFileName);
+
         }
         else if(choice==2)
         {
             string stegoImageFileName;
 
-            SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
             cout<<"provide stego image file name";
             cout<<"\n";
-            SetConsoleTextAttribute(hConsole, savedAttributes);
 
             cin>>stegoImageFileName;
             string extendedStegoImageFileName=addImageFileExtension(stegoImageFileName);
@@ -208,10 +175,8 @@ int main()
 
             if(!inputFile)
             {
-                SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY);
                 cout<<"couldn't find the image file in storage\n";
                 cout<<"redirecting to the option menu\n\n";
-                SetConsoleTextAttribute(hConsole, savedAttributes);
                 continue;
             }
 
@@ -220,24 +185,21 @@ int main()
 
             if(!checkingImageFormat(extendedStegoImageFileName))
             {
-                SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY);
                 cout<<"sorry, the image format is not correct.\n";
                 cout<<"redirecting to the option menu.\n\n";
-                SetConsoleTextAttribute(hConsole, savedAttributes);
                 continue;
             }
 
             extractingData(extendedStegoImageFileName);
+            Sleep(8000);
 
         }
         else if(choice==3)
         {
             string pixelFileName;
 
-            SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
             cout<<"provide pixel file name";
             cout<<"\n";
-            SetConsoleTextAttribute(hConsole, savedAttributes);
 
             cin>>pixelFileName;
             string extendedPixelFileName=addPixelFileExtension(pixelFileName);
@@ -247,20 +209,18 @@ int main()
 
             if(fp==NULL)
             {
-                SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY);
                 cout<<"couldn't find the pixel file in storage\n";
                 cout<<"redirecting to the option menu\n\n";
-                SetConsoleTextAttribute(hConsole, savedAttributes);
                 continue;
             }
 
-            SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
-            cout<<"showing the gif file";
+            cout<<"showing the GIF file";
             cout<<"\n\n\n";
-            SetConsoleTextAttribute(hConsole, savedAttributes);
+
             creatingGif(extendedPixelFileName);
 
             fclose(fp);
+
         }
         else if(choice==4)
         {
@@ -268,13 +228,12 @@ int main()
         }
         else
         {
-            SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE);
             cout<<"Invalid option";
             cout<<"\n\n";
-            SetConsoleTextAttribute(hConsole, savedAttributes);
 
             return EXIT_FAILURE;
         }
+
         system("cls");
     }
 }
