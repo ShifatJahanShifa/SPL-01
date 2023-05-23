b4c27ba771e74631bc746282050e17c550a08021
Shifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2023-05-23 19:16:33 +0600

    modified

---
+++ GIF/header/addGIFFileExtension.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include "header.h"
+
+using namespace std;
+
+string addGIFFileExtension(string GIFFileName)
+{
+    GIFFileName+='.';
+    GIFFileName+='g';
+    GIFFileName+='i';
+    GIFFileName+='f';
+
+    return GIFFileName;
+}
